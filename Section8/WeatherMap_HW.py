2757f1d9c9e0281114bea21fa1d100732656a74b
Sam Casillas <casillas.sammy@gmail.com>
2023-06-09 18:53:35 -0400

    Updating WK8 API Submission Converting JSON into easier to read format

--- Section8/WeatherMap_HW.py
+++ Section8/WeatherMap_HW.py
@@ -16,14 +16,21 @@ api_key = os.getenv('API_KEY')
 
 try:
     #Initiate the data pull request from OpenWeatherMap API
-    data_pull_request = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Orlando&appid='+api_key)
+    data_pull_request = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Memphis&appid='+api_key)
 
     #creating a conditional statement to validate the data pull
     if data_pull_request.status_code == 200:
 
         #Parsing the JSON data pull
         weather_data = json.loads(data_pull_request.text)
-        print(weather_data)
+        
+        #Picking the desired Weather location, Temperature and wind speeds
+        weather_location = weather_data['name']
+        weather_temp = weather_data['main']
+        weather_winds = weather_data['wind']
+        
+        #Printing the weather details output for desired location
+        print(f"City Chosen: {weather_location}, Current Temperature: {weather_temp['temp']}, Current Wind Speeds:{weather_winds['speed']} mph")
     
 
     else:
